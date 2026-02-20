45326ef337664473ebada36644a6dda039e4df80
Bashar Maximous <bashar.maximous@gmail.com>
2026-02-20 23:09:38 +0100

    Update 001_init.sql

--- migrations/001_init.sql
+++ migrations/001_init.sql
@@ -1,11 +1,11 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 CREATE TABLE audit_logs (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     repo_name TEXT NOT NULL,
     pr_number INT NOT NULL,
     status TEXT NOT NULL,
     risk_score INT NOT NULL,
     report JSONB NOT NULL,
-    created_at TIMESTAMPTZ DEFAULT NOW()
+    previous_hash TEXT,
+    current_hash TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
 );
